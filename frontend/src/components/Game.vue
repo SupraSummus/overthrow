43d7d8f854df5a654aff47e43e39e0e7371fa00b
Jan Rydzewski <flegmer@gmail.com>
2020-03-18 23:44:37 +0100

    No more vuetify
    This reverts commit 5bfe385423fd601c39fe8384320071ac6d37f6d8.

--- frontend/src/components/Game.vue
+++ frontend/src/components/Game.vue
@@ -11,6 +11,12 @@
             return false;
           },
         }"
+        @zoom="
+          console.log('zoom');
+          $nextTick(() => {
+            if ($refs.move_menu_slider) $refs.move_menu_slider.refresh();
+          });
+        "
       >
         <div class="grid">
           <map-tile
@@ -38,13 +44,15 @@
 
           <!-- movement command context menu -->
           <context-dialog v-if="move_menu_tile" v-bind="move_menu_tile">
-            <v-slider
+            <vue-slider
               ref="move_menu_slider"
               :value="0"
-              :step="1"
+              :adsorb="true"
+              :interval="1"
               :min="0"
               :max="selected_tile.army"
-              :ticks="true"
+              :lazy="true"
+              :marks="true"
               @change="move"
               @mousedown.native.stop
             />
@@ -90,6 +98,9 @@ export default {
       hovered_tile: null,
       move_menu_tile: null,
       move_amount: 0,
+
+      //debug
+      console: window.console,
     };
   },
   computed: {
@@ -235,6 +246,7 @@ export default {
     },
 
     move: function(amount) {
+      console.log("move", amount);
       if (amount != 0) {
         call_api({
           method: "POST",
@@ -330,6 +342,7 @@ export default {
 <style scoped lang="scss">
 .grid-container {
   position: fixed;
+  z-index: -1;
   overflow: hidden;
   top: 0;
   left: 0;
---
+++ frontend/src/components/Logout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <form @submit.prevent="logout">
+      <p>
+        Logged in as {{ $store.state.user.username }}
+        <button type="submit">Logout</button>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout: function() {
+      this.$store.commit("logged_out");
+    },
+  },
+};
+</script>
